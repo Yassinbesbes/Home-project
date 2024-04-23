49800be0fcbb1e8ad2f3d041f0c6def07c638ca8
yassin besbes <besbesyassin90@gmail.com>
2024-04-24 00:54:35 +0100

    product Page

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,65 +1,112 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Heart from 'react-animated-heart';
 import Image from 'react-bootstrap/Image';
-import imageData from './imageData'; // Importing image data from the same file as Home
-import './product.css'
+import imageData from './imageData';
+import cross from './images/cross.gif';
+import './product.css';
 
 function ProductPage() {
-    const stores = [
-        { name: 'Megastore Tunis Charguia', status: 'En Stock' },
-        { name: 'Magasin Av.liberté Tunis', status: 'En Stock' },
-        { name: 'Magasin Bizerte', status: 'Epuisé' },
-        { name: 'Magasin Sousse', status: 'En Stock' },
-      ];
     const { id } = useParams();
     const [isClick, setClick] = useState(false);
-    const product = imageData[id]; // Retrieve the product data from imageData using the id
+    const product = imageData[id];
 
-    // Ensure product exists before rendering
     if (!product) {
-        return <div>Product not found</div>; 
+        return <div>Product not found</div>;
     }
 
+    const renderProductPlusTable = () => (
+        <table className="product-plus-table">
+            <tbody>
+                {Object.entries(product.plus).map(([key, value]) => (
+                    <tr key={key}>
+                        <td>{key}</td>
+                        <td>{value}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+
     return (
-        <Container style={{marginTop:'25px'}}>
+        <Container style={{ marginTop: '25px' }}>
             <Row>
-                <Col className="Image-product">
-                    <Image className='product-photo' src={product.src} alt={product.title} fluid />
+                <Col md={5} style={{height: "600px" }} className="Image-product">
+                    <Image  className="product-photo" src={product.src} alt={product.title} fluid />
+                    
                 </Col>
-            
-            
-                <Col md={6}>
-                <Row className="product-title-row">
-                    <Col>
-                        <h2>{product.title}</h2>
-                        <h5>{product.price} dt</h5>
-                    </Col>
-                    <Col xs={2} >
-                        <Heart className="heart" isClick={isClick} onClick={() => setClick(!isClick)} fluid />
-                    </Col>
+                
+                <Col md={5}>
+                    <Row style={{ alignItems: 'center',justifyContent:'space-between' }}>
+                        <Col>
+                            <h2 style={{ fontSize: '24px', margin: '0 auto' }}>{product.title}</h2>
+                            <hr />
+                        </Col>
+                        <Col xs={3} >
+                            <Heart style={{height:'25px', width:"25px"}} isClick={isClick} onClick={() => setClick(!isClick)} fluid />
+                        </Col>
+                    </Row>
 
-                </Row> 
+                    <p className="detaille">{product.details}</p>
+                    <hr />
+                    <span style={{ color: 'green' }}>Actuellement indisponible</span><br />
+                    <span className="detaille">
+                        Nous ne savons pas quand cet article sera de nouveau approvisionné ni s'il le sera.
+                    </span>
+                    {renderProductPlusTable()}<hr></hr>
+                    <span className="detaille">
+                        <div style={{marginTop:'-30px'}}
+                        className="product-propos-de-ces-article"
+                        dangerouslySetInnerHTML={{ __html: product.propos_de_ces_article }}
+                        ></div>
+                    </span>
 
-                    <p className='detaille'>{product.details}</p> 
-                    <button className='myCustomButton' >Add to bunner</button>
-
-
-                    <div className="Stock-Position">
-                    <h2 className="titre">Disponibilité des Produits</h2>
-                    <ul className="stock-list">
-                        {stores.map((store) => (
-                        <li key={store.name} className="stock-item">
-                            <span className="store-name">{store.name}</span>
-                            <span className={`stock-status ${store.status === 'En Stock' ? 'in-stock' : 'out-of-stock'}`}>{store.status}</span>
-                        </li>
-                        ))}
-                    </ul>
-                    </div>
                 </Col>
+                <Col style={{ border: '1px solid black', borderRadius: '20px', width: '0px', height:'300px', padding:'20px' }}>
+                    <span style={{paddingTop: "10px", fontSize : "25px"}}>{product.price} dt</span><br></br>
+                    <span style={{ fontSize: "14px"}}>Livraison à 8,15 dt <strong>26 avril - 7 mai</strong></span>
+                    <Col xxl={10}>
+                        <div style={{ display: 'flex', alignItems: 'center' ,justifyContent:'space-between' }}>
+                            <span style={{color: 'red', fontSize: '18px', margin: 0}}>Hors Stock</span>
+                            <img style={{height:'25px', width:'25px', color:'red', float:'right'}} src={cross} alt="cross"/>
+                        </div>
+                    </Col>
+                    <select 
+                style={{
+                    fontSize: '18px',
+                    marginLeft: '5px',
+                    padding: '5px',
+                    borderRadius: '10px',
+                    border: '1px solid #ccc',
+                    backgroundColor: '#f8f8f8',
+                    cursor: 'pointer',
+                    display:'flex',
+                    justifyContent:'center',
+                    alignItems:'center',
+                    margin:'10px auto'
+                }} 
+                value={product.quantity} 
+            >
+                {/* Generate options from 0 to 10 */}
+                {[...Array(5)].map((_, index) => (
+                    <option
+                        key={index}
+                        value={index + 1}
+                        style={{ padding: '5px', backgroundColor: index % 2 === 0 ? '#e6f7ff' : '#ffffff' }}
+                    >
+                        {index+1}
+                    </option>
+                ))}
+            </select>
+        
+                    <button className="myCustomButton">Add to bunner</button>
+                    <button className="buyButton">Buy It</button>
+
+                </Col>
+<hr/>
             </Row>
         </Container>
     );
--- src/imageData.js
+++ src/imageData.js
@@ -1,38 +1,73 @@
-// imageData.js
 import product1 from './images/product1.png';
 import product2 from './images/product2.png';
 import product3 from './images/product3.png';
 import product4 from './images/product4.png';
 
-
-// An array of objects representing different products and their data
 const imageData = [
   {
-    title: 'White Bag',
+    title: 'Salon',
     price: 100.00,
-    src: product1, // Path to the product1 image
-    details: 'There are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization and more compact dimensions, composing sophisticated and exclusive environments.are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, muract  ...'
+    src: product1,
+    details: 'Multiple high-quality pieces, with styles transitioning from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings, and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization, and more compact dimensions to compose sophisticated and exclusive environments.',
+    propos_de_ces_article: `
+    À propos de cet article:
+    <ul>
+        <li>ENSEMBLE SALON DE JARDIN COMPLET: Cet ensemble salon de jardin est composé d'un canapé 2 places, de deux fauteuils et d'une table basse assortie, idéal pour les jardins, terrasses, balcons, etc.</li>
+        <li>GRAND CONFORT: Salon de jardin extérieur 4 personnes doté de 3 coussins rembourrés mousse de 12 cm d'épaisseur grand confort. Coussins zippés déhoussables et lavables pour un nettoyage et entretien faciles.</li>
+        <li>MATÉRIAUX DE QUALITÉ: Ensemble canapé et fauteuils fabriqué en tissu polyester et en acier avec un revêtement en poudre, pour une meilleure résistance face aux rigueurs du climat : rayons UV, humidité, chaleur, etc.</li>
+        <li>PLATEAU TABLE VERRE TREMPÉ: La table d'extérieur est fabriquée avec un plateau à motifs noirs en verre trempé de 5 mm pour ajouter une touche d'élégance à votre patio ou au bord de la piscine.</li>
+        <li>MONTAGE FACILE, RAPIDE: Notice d'assemblage illustrée fournie; dim. fauteuil : 58L x 58l x 66H cm, dim. canapé : 116L x 58l x 66H cm, dim. table : 80L x 42l x 38H cm.</li>
+    </ul>
+`,    plus: {
+      couleur: 'grey',
+      marque: 'Outsunny',
+      taille: '116 x 58 x 66 cm',
+      profondeur: '58 centimètres',
+      dimensions: '116 x 58 x 66 centimètres'
+    }
   },
   {
     title: 'Light Bag',
     price: 45.00,
-    src: product2, // Path to the product2 image
-    details: 'There are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization and more compact dimensions, composing sophisticated and exclusive environments.are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, muract  ...'
+    src: product2,
+    details: 'Multiple high-quality pieces, with styles transitioning from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings, and many gifts to compose great projects.',
+    propos_de_ces_article: 'À propos de cet article: ENSEMBLE SALON DE JARDIN COMPLET: Cet ensemble salon de jardin est composé d\'un canapé 2 places, de deux fauteuils et d\'une table basse assortie, idéal pour les jardins, terrasses, balcons, etc. GRAND CONFORT: Salon de jardin extérieur 4 personnes doté de 3 coussins rembourrés mousse de 12 cm d\'épaisseur grand confort. Coussins zippés déhoussables et lavables pour un nettoyage et entretien faciles. MATÉRIAUX DE QUALITÉ: Ensemble canapé et fauteuils fabriqué en tissu polyester et en acier avec un revêtement en poudre, pour une meilleure résistance face aux rigueurs du climat : rayons UV, humidité, chaleur, etc. PLATEAU TABLE VERRE TREMPÉ: La table d\'extérieur est fabriquée avec un plateau à motifs noirs en verre trempé de 5 mm pour ajouter une touche d\'élégance à votre patio ou au bord de la piscine. Un grand espace vous permettra d\'accueillir de la vaisselle et plus encore, facile à nettoyer après le dîner. MONTAGE FACILE, RAPIDE: Notice d\'assemblage illustrée fournie; - dim. fauteuil : 58L x 58l x 66H cm, dim. canapé : 116L x 58l x 66H cm, dim. table : 80L x 42l x 38H cm.',
+    plus: {
+      couleur: 'white',
+      marque: 'BrandName',
+      taille: '40 x 20 x 15 cm',
+      profondeur: '15 centimètres',
+      dimensions: '40 x 20 x 15 cm'
+    }
   },
   {
     title: 'White Bag',
     price: 100.00,
-    src: product3, // Path to the product3 image
-    details: 'There are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization and more compact dimensions, composing sophisticated and exclusive environments.are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, muract  ...'
+    src: product3,
+    details: 'Multiple high-quality pieces, with styles transitioning from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings, and many gifts to compose great projects.',
+    propos_de_ces_article: 'À propos de cet article: ENSEMBLE SALON DE JARDIN COMPLET: Cet ensemble salon de jardin est composé d\'un canapé 2 places, de deux fauteuils et d\'une table basse assortie, idéal pour les jardins, terrasses, balcons, etc. GRAND CONFORT: Salon de jardin extérieur 4 personnes doté de 3 coussins rembourrés mousse de 12 cm d\'épaisseur grand confort. Coussins zippés déhoussables et lavables pour un nettoyage et entretien faciles. MATÉRIAUX DE QUALITÉ: Ensemble canapé et fauteuils fabriqué en tissu polyester et en acier avec un revêtement en poudre, pour une meilleure résistance face aux rigueurs du climat : rayons UV, humidité, chaleur, etc. PLATEAU TABLE VERRE TREMPÉ: La table d\'extérieur est fabriquée avec un plateau à motifs noirs en verre trempé de 5 mm pour ajouter une touche d\'élégance à votre patio ou au bord de la piscine. Un grand espace vous permettra d\'accueillir de la vaisselle et plus encore, facile à nettoyer après le dîner. MONTAGE FACILE, RAPIDE: Notice d\'assemblage illustrée fournie; - dim. fauteuil : 58L x 58l x 66H cm, dim. canapé : 116L x 58l x 66H cm, dim. table : 80L x 42l x 38H cm.',
+    plus: {
+      couleur: 'white',
+      marque: 'BrandName',
+      taille: '40 x 25 x 15 cm',
+      profondeur: '15 centimètres',
+      dimensions: '40 x 25 x 15 cm'
+    }
   },
   {
-    title: 'Light Bag',
+    title: 'Dark Bag',
     price: 45.00,
-    src: product4, // Path to the product4 image
-    details: 'There are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization and more compact dimensions, composing sophisticated and exclusive environments.are multiples of high quality pieces, with styles that transition from classic to contemporary. An exclusive selection of lampshades, vases, muract  ...'
-  },
-  // You can add more product data here
+    src: product4,
+    details: 'Multiple high-quality pieces, with styles transitioning from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings, and many gifts to compose great projects.',
+    propos_de_ces_article: 'À propos de cet article: ENSEMBLE SALON , de deux fauteuils et d\'une table basse assortie, idéal pour les jardins, terrasses, balcons, etc.',
+    plus: {
+      couleur: 'black',
+      marque: 'BrandName',
+      taille: '30 x 15 x 10 cm',
+      profondeur: '10 centimètres',
+      dimensions: '30 x 15 x 10 cm'
+    }
+  }
 ];
 
-// Export the array as the default export
 export default imageData;
