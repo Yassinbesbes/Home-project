5f2a69f203468cad506efc74d7c6281b669e359c
yassin besbes <besbesyassin90@gmail.com>
2024-05-07 23:45:53 +0100

    some update collection

--- src/collection.js
+++ src/collection.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import imageData from './imageData';
 import { useNavigate } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
 
 function Collection() {
     const navigate = useNavigate();
@@ -12,7 +16,7 @@ function Collection() {
 
     return (
         <div>
-            <Container >
+            <Container style={{marginTop:"50px"}} >
                 {/* Loop to display the message three times */}
                 {[...Array(3)].map((_, i) => (
                 
@@ -38,6 +42,15 @@ function Collection() {
                 </Row>
                 ))}
             </Container>
+            <footer>
+            <Box sx={{ flexGrow: 1 }}>
+            <Grid display="flex" justifyContent="center" alignItems="center" margin="50px">
+            <Stack >
+                <Pagination count={5} color="grey" />
+            </Stack>
+            </Grid>
+            </Box>
+            </footer>
         </div>
     );
 }
--- src/imageData.js
+++ src/imageData.js
@@ -6,9 +6,9 @@ import product4 from './images/product4.png';
 const imageData = [
   {
     title: 'Salon',
-    price: 99.98,
+    price: 199.98,
     src: product1,
-    details: 'Multiple high-quality pieces, with styles transitioning from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings, and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization, and more compact dimensions to compose sophisticated and exclusive environments.',
+    details: 'Multiple high-quality pieces, with styles transitioning from classic to contemporary. An exclusive selection of lampshades, vases, murals, pillows, paintings, and many gifts to compose great projects. In order selection, a mix of basic style, stricter customization, and more compactand more compactand more compactand more compact dimensions to es, with styles transitioning from classic to contemporary. An exclusive selection of la es, with styles transitioning from classic to contemporary. An exclusive selection of lacompose sophisticated and exclusive environments .',
     propos_de_ces_article: `
     
     <ul>
